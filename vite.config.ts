c661b8af55e0a2eb9ce6e8c00aefbfbe21c84464
gamexlabtr <164689569+gamexlabtr@users.noreply.github.com>
2026-06-19 18:17:18 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/repo-adin/', // GitHub kullanıcı adın ve repo adın aynı olmalı
+  base: '/gamexlabtr/', // gamexlabtr/bombardment
 })
