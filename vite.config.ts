c225b5163cfaf49359464f33d6c86e8dde8c59ac
gamexlabtr <164689569+gamexlabtr@users.noreply.github.com>
2026-06-30 01:25:22 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
-  // GitHub Pages alt klasör yapısı için bu satır ŞARTTIR:
+  // Hem GitHub Pages hem StackBlitz alt klasör yolları için bu şarttır:
   base: "./", 
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  // Ağırlaştıran ve kilitlenmeye sebep olan singleFile eklentisini kaldırdık
+  plugins: [react(), tailwindcss()], 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
