02846d3087dc606e2926b4c6076a7a02922a5dcb
gamexlabtr <164689569+gamexlabtr@users.noreply.github.com>
2026-06-30 01:21:47 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages alt klasör yapısı için bu satır ŞARTTIR:
+  base: "./", 
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
